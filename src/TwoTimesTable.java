6d30e2f94859af098fbde3f7646e30ae7addaed6
Nirali0327 <nirali0327@gmail.com>
2022-05-01 15:17:25 +0100

    my home work commit

---
+++ src/TwoTimesTable.java
@@ -0,0 +1,12 @@
+public class TwoTimesTable {
+    public static void main(String[] args) {
+
+        int num = 2, i = 1;
+        while (i <= 12) {
+            System.out.println(num+ " * " + i +" =  "+num *i);
+            i++;
+        }
+    }
+
+}
+
